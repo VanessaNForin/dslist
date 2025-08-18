36dadd0e1118341921c3ea42ce01f13bff65e505
Vanessa Forin <dev.vanessanforin@outlook.com>
2025-08-18 17:22:37 -0300

    Game Seed

--- src/main/java/com/forinvanessa/dslist/entities/Game.java
+++ src/main/java/com/forinvanessa/dslist/entities/Game.java
@@ -22,22 +22,28 @@ public class Game {
 	@Column(name = "game_year") //Define como o nome vai aparecer na tabela de dados
 	private Integer year; //Ano
 	private String genre; //Genero
-	private String platform; //Plataform
-	private String imgUrl; //Imagem
+	private String platforms; //Plataform
+	private Double score;
+	private String imgUrl; //Imagem]
+	
+	@Column(columnDefinition = "TEXT") //Transforma String em Texto acima de 255 caracteres
 	private String shortDescription; //Descrição resumida
+	
+	@Column(columnDefinition = "TEXT") //Transforma String em Texto acima de 255 caracteres
 	private String longDescription; //Descrição detalhada
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -78,13 +84,22 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
+	}
+	
+	public void setPlatforms(String platform) {
+		this.platforms = platform;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public Double getScore() {
+		return score;
 	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
 
 	public String getImgUrl() {
 		return imgUrl;
